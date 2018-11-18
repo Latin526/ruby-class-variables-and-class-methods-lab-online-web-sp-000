cc3ef12d1718569cb4a3edc46901723579911d99
Yasmin Torres <yasmintorres90@hotmail.com>
2018-11-18 19:28:49 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,10 @@
 class Song 
    attr_accessor :name, :artist, :genre	
+   
    @@count = 0	
   @@artists = []	
   @@genres = []	
+  
    def initialize(name, artist, genre)	
     @name = name	
     @artist = artist	
@@ -11,12 +13,15 @@ class Song
     @@genres << genre	
     @@artists << artist	
   end	
+  
    def self.count	
     @@count	
   end	
+  
    def self.artists	
     @@artists.uniq	
   end	
+  
    def self.artist_count	
     artist_count = {}	
     @@artists.each do |artist|	
@@ -28,9 +33,11 @@ class Song
     end	
     artist_count	
   end	
+  
    def self.genres	
     @@genres.uniq	
   end	
+  
    def self.genre_count	
     genre_count = {}	
     @@genres.each do |genre|	
